a4b520566579bfcbb85f97d7aada9c7fe0d59764
rtrinu <rbinu2004@gmail.com>
2025-02-16 13:26:18 +0000

    Final Commit, Second Tutorial Project Completed

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+    int secertNumber = 5;
+    int guess;
+    int guessLimit = 3;
+    int guessCounter = 0;
+    while (guess != secertNumber && guessCounter < guessLimit ) {
+        printf("Guess the number between 1 and 10: \n");
+        scanf("%d", &guess);
+        if (guess == secertNumber) {
+            printf("You win!\n");
+
+        } else{
+            printf("You lose!\n");
+            guessCounter++;
+        }
+    }
     return 0;
 }
